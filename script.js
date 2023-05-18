7b3893064013f5c7b4130a66f8e779825c123a05
santiago510 <santiagoiannucci@gmail.com>
2023-05-18 09:39:28 -0300

    relojes mundiales v.1

---
+++ script.js
@@ -0,0 +1,49 @@
+function updateClocks() {
+    // Obtener la hora actual
+    var date = new Date();
+
+    // Calcular la hora en diferentes zonas horarias
+    var timezoneOffset1 = -3;  // Ajuste horario para Argentina (UTC-3)
+    var timezoneOffset2 = -5;  // Ajuste horario para México (UTC-5)
+    var timezoneOffset3 = 2;   // Ajuste horario para España (UTC+2)
+    var timezoneOffset4 = -6;  // Ajuste horario para Costa Rica (UTC-6)
+    var timezoneOffset5 = -5;  // Ajuste horario para Ecuador (UTC-5)
+
+    var hours1 = date.getUTCHours() + timezoneOffset1;
+    var minutes1 = date.getUTCMinutes();
+    var seconds1 = date.getUTCSeconds();
+
+    var hours2 = date.getUTCHours() + timezoneOffset2;
+    var minutes2 = date.getUTCMinutes();
+    var seconds2 = date.getUTCSeconds();
+
+    var hours3 = date.getUTCHours() + timezoneOffset3;
+    var minutes3 = date.getUTCMinutes();
+    var seconds3 = date.getUTCSeconds();
+
+    var hours4 = date.getUTCHours() + timezoneOffset4;
+    var minutes4 = date.getUTCMinutes();
+    var seconds4 = date.getUTCSeconds();
+
+    var hours5 = date.getUTCHours() + timezoneOffset5;
+    var minutes5 = date.getUTCMinutes();
+    var seconds5 = date.getUTCSeconds();
+
+    // Actualizar los relojes en la página
+    document.getElementById('clock1').textContent = 'Argentina: ' + formatTime(hours1, minutes1, seconds1);
+    document.getElementById('clock2').textContent = 'México: ' + formatTime(hours2, minutes2, seconds2);
+    document.getElementById('clock3').textContent = 'España: ' + formatTime(hours3, minutes3, seconds3);
+    document.getElementById('clock4').textContent = 'Costa Rica: ' + formatTime(hours4, minutes4, seconds4);
+    document.getElementById('clock5').textContent = 'Ecuador: ' + formatTime(hours5, minutes5, seconds5);
+}
+
+function formatTime(hours, minutes, seconds) {
+    return padZero(hours) + ':' + padZero(minutes) + ':' + padZero(seconds);
+}
+
+function padZero(value) {
+    return value < 10 ? '0' + value : value;
+}
+
+// Actualizar los relojes cada segundo
+setInterval(updateClocks, 1000);
